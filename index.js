e2639940fdb69e67f7c518a8a69ce5b818c70f85
jago1009 <garwoodj22@gmail.com>
2025-04-03 11:56:07 -0400

    Add image gallery functionality with overlay and navigation enhancements

---
+++ index.js
@@ -0,0 +1,33 @@
+function initOverlay() {
+    $('#overlay').on('click', function () {
+        $('#overlay').fadeOut(200,function () {
+            $(this).remove();
+        });
+    });
+}
+
+$('#gallery-center').on('click', function (e) {
+    e.preventDefault();
+    var target = $(e.target);
+    var imgSrc = target.attr('src');
+    var imgAlt = target.attr('alt');
+
+    $('body').append(`<div id="overlay"><img class="overlay-image" src="${imgSrc}"</div>`);
+    $('#overlay').fadeIn(200);
+    initOverlay();
+})
+$('.gallery-slide').on('click', function (e) {
+var target = $(e.target);
+var parent = target.parent();
+
+$('#current').removeAttr('id');
+parent.attr('id', 'current');
+
+$('#gallery-center').animate({
+    opacity: 0
+},200, function () {
+    $(this).attr('src', target.attr('src'));
+    $(this).animate({opacity: 1}, 200);
+})
+
+})
